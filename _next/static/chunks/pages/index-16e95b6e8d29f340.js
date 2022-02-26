fe1ce9f849f40f3de2a2ff8cf6077b427742975d
Mikola  Parfenyuck <mikola.parfenyuck@gmail.com>
2022-02-26 15:21:29 +0200

    Updates

---
+++ _next/static/chunks/pages/index-16e95b6e8d29f340.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5301:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(2119)}])}},function(n){n.O(0,[178,119,774,888,179],(function(){return _=5301,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
