54bfb41d08546a65a318de6c1cae4643ea05bd23
Mikola  Parfenyuck <mikola.parfenyuck@gmail.com>
2025-01-07 16:39:11 +0200

    Updates

---
+++ _next/static/G6oHp0APUNYEUzmDJI3DQ/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fspread-the-word","\u002F[lang]\u002Fp\u002F[segment]","\u002Fp\u002F[segment]","\u002F[lang]\u002Fspread-the-word","\u002F[lang]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
